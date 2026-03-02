e182adee8f8410ca39547a68916f95d2126239ae
Theo <git@theo.is-a.dev>
2026-03-02 06:02:57 +0300

    adjust note sizes

--- note_overlay.gd
+++ note_overlay.gd
@@ -2,15 +2,15 @@ extends Node
 
 @export var note_colors : Array[Color]
 
-var notes = []
+var notes: Array[Control] = []
 
 var midi_player: MidiPlayer
 var asp: AudioStreamPlayer
 
-@export var width: int = 160
-@export var height: int = 20
+@export var width: int = 128
+@export var height: int = 15
 @export var distance: int = 5
-@export var note_offset: int = 30
+@export var note_offset: int = 26
 @export var speed: int = 100
 
 var playing: bool = false
@@ -27,38 +27,45 @@ func _ready():
 	midi_player.midi = midi_resource
 	midi_player.link_audio_stream_player([asp])
 	
-	var microseconds_per_tick = midi_resource.tempo / midi_resource.division;
-	var speed_scale = 1
+	var microseconds_per_tick: float = midi_resource.tempo / midi_resource.division;
+	var speed_scale: float = 1
 	
-	var notes_on = []
-	
+	var notes_on: Array[Variant] = []
 	for track in midi_resource.tracks:
+		var last_note: Variant = null
 		var time: float = 0
-		var last_note: Variant
 		
 		for event in track.events:
 			if event['type'] == 'note':
 				var event_delta: float = event['delta'] * microseconds_per_tick;
 				var event_delta_seconds: float = event_delta / 1000000.0;
+				
 				event_delta_seconds /= speed_scale;
 				time += event_delta_seconds
 				
 				if event.subtype == MIDI_MESSAGE_NOTE_ON:
-					last_note = { "track": event.track, "note": event.note, "time": time, "duration": time - last_note.time if last_note else 0 }
+					last_note = { 
+						"track": event.track, 
+						"note": event.note, 
+						"time": time, 
+						"duration": (time - last_note.time if last_note else 0.1)
+					}
+					
 					notes_on.append(last_note)
 				
 				if event.subtype == MIDI_MESSAGE_NOTE_OFF:
 					last_note.duration = time - last_note.time
 	
-	for note_data in notes_on:
+	for note_data: Variant in notes_on:
 		var track = note_data.track
 		var note = note_data.note
 		var note_time = note_data.time
 		var duration = note_data.duration
 		
-		var box = ColorRect.new()
+		var box: ColorRect = ColorRect.new()
 		box.color = note_colors[track]
 		box.size = Vector2(width * duration, height)
+		
 		self.add_child(box)
 		
 		box.position.x = self.size.x / 2 - (note_time * width)
@@ -68,7 +75,7 @@ func _ready():
 	midi_player.play()
 
 func _process(delta):
-	for note in notes:
+	for note: Control in notes:
 		note.position.x += delta * speed
 		
 		if note.position.x > self.size.x:
