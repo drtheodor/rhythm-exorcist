9f70d893251ef4261f8575fd973e3d834d22a256
BatmenzDW <turtleterracotta@gmail.com>
2026-03-09 15:02:57 -0400

    Title Screen script

---
+++ scripts/titlescreen.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+const GAMESCENE = preload("uid://cmuevhd5wo1mp")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stage_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
