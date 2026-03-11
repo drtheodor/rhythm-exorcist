8a71811f06a4f90bb9d121c01996a4c36f4a69cc
Theo <git@theo.is-a.dev>
2026-03-11 22:52:33 +0300

    feat: midi stuff thingy

--- scripts/key_listener.gd
+++ scripts/key_listener.gd
@@ -1,57 +1,12 @@
 extends Sprite2D
 
-const KEY = preload("res://scenes/objects/key_running.tscn")
 @export var key_name: String = ""
 
-var key_running_queue = []
-var current_key
-
-@export var hit_threshold: float = 16.0 
-@export var perfect_threshold: float = 6.0
-
-@export var fear: int = 5
-@export var perfect_heal: int = 1
-
 func _process(_delta: float) -> void:
-	if current_key and current_key.has_passed:
-		self._pick_key()
-		GameManager.fear += self.fear
-		return
-
 	if Input.is_action_just_pressed(key_name):
 		var tween = self.create_tween()
 		
 		tween.tween_property(self, "offset:y", -3, 0.1).set_trans(Tween.TRANS_SINE)
 		tween.tween_property(self, "offset:y", 0, 0.1).set_trans(Tween.TRANS_SINE)
 		
-		if current_key:
-			var distance = abs(current_key.pass_threshold - current_key.position.x)
-			
-			if distance <= hit_threshold:
-				if distance <= perfect_threshold:
-					GameManager.fear -= self.perfect_heal
-				
-				self._pick_key();
-				$AnimationPlayer.play("hand_hitting")
-
-func _create_key():
-	var kr_inst = KEY.instantiate()
-	self.get_tree().get_root().add_child(kr_inst)
-	
-	kr_inst.setup(position.y)
-	key_running_queue.append(kr_inst)
-
-func _pick_key():
-	if self.current_key:
-		self.current_key.queue_free()
-	
-	self.current_key = key_running_queue.pop_front()
-
-func _on_random_spawn_timer_timeout() -> void:
-	self._create_key()
-	
-	if not current_key:
-		self._pick_key()
-	
-	$RandomSpawnTimer.wait_time = randf_range(0.4, 3)
-	$RandomSpawnTimer.start()
+		$AnimationPlayer.play("hand_hitting")
---
+++ scripts/midi_manager.gd
@@ -0,0 +1,158 @@
+extends MidiPlayer
+
+const KEY = preload("res://scenes/objects/key_running.tscn")
+
+#@export var note_colors : Array[Color] = [
+	#Color.hex(0xff4747),
+	#Color.hex(0x2cff2c),
+	#Color.hex(0x4343ff),
+#]
+
+@onready var running_parent = $Running
+
+@export var audio: AudioStream
+@export var note_height: int = 16
+@export var note_width: int = 4
+@export var trigger_line_x: float = 70
+@export var perfect_line_x: float = 66.0
+@export var play_line_x: float = 64.0
+@export var play_line_y: float = 144.0
+@export var debug: bool = false
+
+@export var perfect_threshold: float = 1
+@export var fear: int = 5
+@export var perfect_heal: int = 1
+
+@export var keys: Array[String] = ["up", "down"]
+
+var notes: Array = []
+
+func _ready() -> void:
+	var asp = $AudioStreamPlayer
+	asp.stream = self.audio
+	
+	self.link_audio_stream_player([asp])
+	
+	# Calculate tempo-based pixels per second
+	#var beats_per_second = 1000000.0 / self.midi.tempo
+	
+	var microseconds_per_tick: float = (self.midi.tempo as float) / self.midi.division
+	var seconds_per_tick: float = microseconds_per_tick / 1000000.0
+	
+	var temp_notes = []
+	
+	for track in self.midi.tracks:
+		var last_note: Variant = null
+		var time: float = 0
+		
+		for event in track.events:
+			time += event.delta * seconds_per_tick
+			if event['type'] == 'note':
+				if event.subtype == MIDI_MESSAGE_NOTE_ON:
+					last_note = { 
+						"track": event.track, 
+						"note": event.note, 
+						"time": time, 
+						"duration": 0#(time - last_note.time if last_note else 0.1)
+					}
+					
+					temp_notes.append(last_note)
+				
+				if event.subtype == MIDI_MESSAGE_NOTE_OFF:
+					last_note.duration = time - last_note.time
+					last_note = null
+	
+	temp_notes.sort_custom(func(a, b): return a.time < b.time)
+	
+	for note in temp_notes:
+		create_note(note)
+	
+	if debug:
+		$NoteHider.hide()
+		draw_play_line()
+	
+	self.play()
+	
+	self.finished.connect(_on_finished)
+
+func _process(_delta: float) -> void:
+	if self.get_state() != 0: # only when playing
+		return
+
+	for note in notes:
+		var note_start_time = note.get_meta("start_time")
+		
+		var d = (current_time) / (note_start_time) if note_start_time else 2
+		var maxx = get_viewport().get_visible_rect().size.x
+		
+		# Do not question.
+		note.position.x = (maxx + play_line_x) * (1 - d) + 2*play_line_x
+		
+		#print(note.get_meta("note"), "/", "start: ", note_start_time, "; cur: ", current_time, "; d: ", d, "; x: ", note.position.x)
+	
+	self.notes = notes.filter(func(note):
+		if note.position.x + note_width < trigger_line_x:
+			if Input.is_action_just_pressed(keys[note.get_meta("track") - 1]):
+				var distance = abs(perfect_line_x - note.position.x)
+				
+				if distance <= perfect_threshold:
+					GameManager.fear -= self.perfect_heal
+				
+				note.queue_free()
+				return false
+			
+			if note.position.x + note_width < play_line_x:
+				GameManager.fear += self.fear
+				note.queue_free()
+				return false
+		return true
+	)
+
+func create_note(note_data: Dictionary):
+	#var box: ColorRect = ColorRect.new()
+	#box.color = note_colors[note_data.track % note_colors.size()]
+	
+	# Width based on duration and pixels per second
+	#box.size = Vector2(note_data.duration * pixels_per_second, note_height)
+	var box = KEY.instantiate()
+	self.running_parent.add_child(box)
+	
+	# Store metadata
+	box.set_meta("start_time", note_data.time)
+	box.set_meta("duration", note_data.duration)
+	box.set_meta("track", note_data.track)
+	box.set_meta("note", note_data.note)
+	
+	# Initial position (will be updated in _process)
+	box.position.y = (note_data.track - 1) * note_height + play_line_y
+	
+	notes.append(box)
+
+func draw_play_line():
+	var y = play_line_y - note_height / 2.
+	var h = keys.size() * note_height
+	
+	# Create a visual line at the play position
+	var line = ColorRect.new()
+	line.color = Color.WHITE
+	line.size = Vector2(2, h)
+	line.position = Vector2(play_line_x - 2, y)
+	add_child(line)
+
+	line = ColorRect.new()
+	line.color = Color.PURPLE
+	line.size = Vector2(2, h)
+	line.position = Vector2(trigger_line_x - 2, y)
+	add_child(line)
+	
+	line = ColorRect.new()
+	line.color = Color.GREEN
+	line.size = Vector2(2, h)
+	line.position = Vector2(perfect_line_x - 2, y)
+	add_child(line)
+
+func _on_finished():
+	for note in self.notes:
+		note.queue_free()
+	
+	notes.clear()
