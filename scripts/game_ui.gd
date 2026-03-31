96f0a23186ee8f1d187f3a7406c497d816a570a1
Theo <git@theo.is-a.dev>
2026-03-31 16:22:45 +0300

    fix: update possession bar on miss as well

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -23,10 +23,10 @@ const SYMBOL_HEIGHT: int = 24
 
 func _ready() -> void:
 	GameManager.on_fear.connect(self._on_fear)
-	GameManager.on_faith.connect(_on_faith)
-	GameManager.on_game_over.connect(_on_game_over_notified)
-	GameManager.go_interstage.connect(_in_scene_dialogue)
-	GameManager.note_hit.connect(_on_note_hit)
+	GameManager.on_faith.connect(self._on_faith)
+	GameManager.on_game_over.connect(self._on_game_over_notified)
+	GameManager.go_interstage.connect(self._in_scene_dialogue)
+	GameManager.note_hit.connect(self._on_note_hit)
 	faith_bar.value = GameManager.faith
 	poss_bar.value = 100
 	var should_slide = GameManager.animated_level_entry
@@ -38,14 +38,14 @@ func _ready() -> void:
 			.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
 
 func _on_note_hit() -> void:
-	var midi_player = get_tree().get_first_node_in_group("MidiPlayer") as MidiManager
-	if midi_player and midi_player.song_duration > 0.0:
-		poss_bar.value = (1.0 - midi_player.current_time / midi_player.song_duration) * 100.0
+	self._update_poss_bar()
 
 func _on_fear(incr: int):
 	fear_bar.value = clamp(fear_bar.value + incr, 0, 100)
 	if incr > 0:
 		_shake_ui()
+	
+	self._update_poss_bar()
 
 func _shake_ui() -> void:
 	var tween = create_tween()
@@ -53,6 +53,11 @@ func _shake_ui() -> void:
 	tween.tween_property(canvas_layer, "offset:x", -2.0, 0.03)
 	tween.tween_property(canvas_layer, "offset:x", 1.0, 0.03)
 	tween.tween_property(canvas_layer, "offset:x", 0.0, 0.03)
+
+func _update_poss_bar() -> void:
+	var midi_player = get_tree().get_first_node_in_group("MidiPlayer") as MidiManager
+	if midi_player and midi_player.song_duration > 0.0:
+		poss_bar.value = (1.0 - midi_player.current_time / midi_player.song_duration) * 100.0
 
 func _on_faith(new_val: int) -> void:
 	faith_bar.value = new_val
